a4f966be91e5ac061e256f5461c22ce24ad1fa31
max.rothman <max.rothman@vecna.com>
2014-09-17 13:12:00 -0400

    Drawing circles!

--- xss_colorsquares.py
+++ xss_colorsquares.py
@@ -1,75 +1,72 @@
 #!/usr/bin/python
+#lovingly ripped off of <www.thok.org/intranet/python/exif/xss_colorsquares.py>
 
 import Xlib.display
 import random
 import time
 import os
 
-import Image
 
-# Quoth http://www.dis.uniroma1.it/~liberato/screensaver/simplesquares.html
-#
-#    1. Open the display: this is used to connect to the X server, and
-#       allows the program to draw.
+CELL_SIZE = 10   #in pixels
 
-def run_saver(window=None):
-    """main loop of a screensaver"""
 
-    # load it
-    myface = Image.open(os.path.expanduser("~/.zpix/eichin.jpg"))
+class ScreenSaver(object):
+  def __init__(self, window):
+  # Quoth http://www.dis.uniroma1.it/~liberato/screensaver/simplesquares.html
+  #
+  #    1. Open the display: this is used to connect to the X server, and
+  #       allows the program to draw.
+    self.display = Xlib.display.Display()
 
-    display = Xlib.display.Display()
-
-#    2. Determine the root window: this is the window that is used by
-#       xscreensaver modules as a canvas for drawing.
-    rootscreen = display.screen()
+  #    2. Determine the root window: this is the window that is used by
+  #       xscreensaver modules as a canvas for drawing.
+    self.rootscreen = self.display.screen()
     if window:
-        window = int(window, 16)
-        window = display.create_resource_object('window', window)
+      window = int(window, 16)
+      self.window = self.display.create_resource_object('window', window)
     else:
-        window = rootscreen.root
+      self.window = rootscreen.root
 
-#    3. Create a graphic context: a graphic context tells how drawing
-#       is made (for example, it contains the foreground color).
+  #    3. Create a graphic context: a graphic context tells how drawing
+  #       is made (for example, it contains the foreground color).
+    self.gc = self.window.create_gc()
 
-    gc = window.create_gc(
-        foreground = rootscreen.white_pixel,
-        )
+    # initialize grid-to-pizel map
+    self.xmax = self.rootscreen.width_in_pixels / CELL_SIZE
+    self.ymax = self.rootscreen.height_in_pixels / CELL_SIZE
+    self.store = set()
 
-#    4. Draw something.
+    self.main()
 
+  def render(self, x, y):
+    self.store.remove((x,y))
+    self.window.fill_arc(self.gc,
+        x*CELL_SIZE, y*CELL_SIZE),  #x, y
+        CELL_SIZE, CELL_SIZE,       #width, height
+        0, 360*64)                  #rotation, 1/64ths of degrees to draw
+    self.display.flush()
+
+  def main(self):
     while True:
+      # random color
+      new_pixel = random.randint(0, 2**24)
+      self.gc.change(foreground=new_pixel)
+      self.window.fill_arc(self.gc,
+               random.randint(0, self.rootscreen.width_in_pixels), # x
+               random.randint(0, self.rootscreen.height_in_pixels), # y
+               100, # width
+               100, # height,
+               0, 360*64
+               )
+      
+      if random.randint(0, 500) < 1:
+        self.window.clear_area(0, 0, 0, 0) # clear_window
+  #    5. Flush the output (make drawing visible).
+      self.display.flush()
+      time.sleep(0.1) # not 10us...
 
-        if random.randint(0, 10) < 6:
-            # random color
-            new_pixel = random.randint(0, 2**24)
-            gc.change(foreground=new_pixel)
-            window.rectangle(gc,
-                             random.randint(0, rootscreen.width_in_pixels), # x
-                             random.randint(0, rootscreen.height_in_pixels), # y
-                             50, # width
-                             40, # height
-                             )
-        else:
-            # ideas (but not code) from:
-            # http://www.koders.com/python/fidC379BA6783F3A055BA9D24B14438390CE4B70769.aspx
-            # Circus, csetroot.py; turns out that code is in Xlib drawable.py now.
-            assert myface.mode == "RGB", "wrong PIL mode, only ZPixmap implemented"
-            window.put_pil_image(gc,
-                                 random.randint(0, rootscreen.width_in_pixels), # x
-                                 random.randint(0, rootscreen.height_in_pixels), # y
-                                 myface,
-                                 )
-        if random.randint(0, 500) < 1:
-            window.clear_area(0, 0, 0, 0) # clear_window
-#    5. Flush the output (make drawing visible).
-        display.flush()
-        time.sleep(0.01) # not 10us...
-
-
-    display.flush()
 
 if __name__ == "__main__":
-    run_saver(os.getenv('XSCREENSAVER_WINDOW'))
-    # see http://www.dis.uniroma1.it/~liberato/screensaver/install.html
-    # for how to configure your .xscreensaver entry
+  ScreenSaver(os.getenv('XSCREENSAVER_WINDOW'))
+  # see http://www.dis.uniroma1.it/~liberato/screensaver/install.html
+  # for how to configure your .xscreensaver entry
